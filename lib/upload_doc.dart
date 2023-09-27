7f849ba0c90dc56fc5831ad32b09a5bd7ce7654d
ghanshyam <ghanshyambendkoli@gmail.com>
2023-09-27 22:17:21 +0530

    font added

---
+++ lib/upload_doc.dart
@@ -0,0 +1,42 @@
+
+import 'package:flutter/material.dart';
+
+class UploadDoc extends StatefulWidget {
+  const UploadDoc({super.key});
+
+  @override
+  State<UploadDoc> createState() => _UploadDocState();
+}
+
+class _UploadDocState extends State<UploadDoc> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Activate Future & Options'),
+        
+      ),
+      body:  Padding(padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 215,
+              decoration: BoxDecoration(
+             border: Border.all(color: const Color(0xFFececec)),
+             borderRadius: const BorderRadius.all(Radius.circular(4))
+             ),
+             child: const Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [Text("Select income proof",style: TextStyle(fontSize: 14,fontWeight: FontWeight.w500),)
+              
+              ],
+             ),
+            ),
+            
+          ],
+      ),
+      ),
+    );
+  }
+}
